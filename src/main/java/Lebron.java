673ab37831261207e8f9ecd8f10845b8e981ebf0
Simantak Dabhade <dsimantak@gmail.com>
2025-08-19 12:52:58 +0800

    feat: renamed the project to lebron and added the welcome and goodbye message

---
+++ src/main/java/Lebron.java
@@ -0,0 +1,19 @@
+public class Lebron {
+    public static void main(String[] args) {
+        String welcomeMsg = """
+                ____________________________________________________________
+                Hello! I'm lebron (but you can call me GOAT or leking, even pookiebron if your feeling like it)
+                What can I do for you?
+                ____________________________________________________________
+                """;
+
+        String goodbyeMsg = """
+                ___________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+
+        System.out.println(welcomeMsg);
+        System.out.println(goodbyeMsg);
+    }
+}
