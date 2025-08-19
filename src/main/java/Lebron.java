368919ca3b55bf618008923c8e3db27ab33e48c3
Simantak Dabhade <dsimantak@gmail.com>
2025-08-19 13:01:13 +0800

    feat: lebron now echos messages until you say bye then the program exits, we use the scanner library to do this

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
+        Lebron lebron = new Lebron();
+        lebron.welcomeMessage();
+        lebron.chatLoop();
+        lebron.goodbyeMessage();
+    }
+
+    private void welcomeMessage() {
         String welcomeMsg = """
                 ____________________________________________________________
                 Hello! I'm lebron (but you can call me GOAT or leking, even pookiebron if your feeling like it)
@@ -7,13 +16,37 @@ public class Lebron {
                 ____________________________________________________________
                 """;
 
+        System.out.println(welcomeMsg);
+    }
+
+    private void goodbyeMessage() {
         String goodbyeMsg = """
                 ___________________________________________________________
                  Bye. Hope to see you again soon!
                 ____________________________________________________________
                 """;
 
-        System.out.println(welcomeMsg);
         System.out.println(goodbyeMsg);
     }
+
+    private void chatLoop() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("You: ");
+            String input = scanner.nextLine();
+
+            // breaking out if the person types bye
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println("Le-king: " + input); // echo back
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
+    }
+
 }
